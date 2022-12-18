2de268a8af2eecd45cc701318d03da7027a4f2f0
Denis Rahmadi <93427155+denisrahmadi@users.noreply.github.com>
2022-12-18 22:08:27 +0700

    Practice Component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <InputTodo></InputTodo>
+    <!-- Start Todo List -->
+    <TodoList></TodoList>
+    <!-- End Todo List -->
+  </div>
+</template>
+<script>
+import InputTodo from "./components/InputTodo.vue";
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: {
+    InputTodo,
+    TodoList
+  }
+};
+</script>
+<style>
+/** Body Style **/
+body {
+  font-family: "Open Sans", sans-serif;
+}
+/** Form Style **/
+
+/** Start Todo List **/
+/** End Todo List*/
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div>
+        <div class="todos">
+            <div class="todo">
+                <h2 class="todo_description">Go to gym</h2>
+                <p>Go to celebrity gymn</p>
+                <a><i class="fa-regular fa-circle-check"></i></a>
+                <a><i class="fa-solid fa-trash "></i></a>
+            </div>
+        </div>
+  </div>
+</template>
+<script>
+export default {
+
+};
+</script>
+
+<style lang="">
+.todos_container {
+  display: flex;
+  justify-content: center;
+  margin-top: 35px;
+  margin-bottom: 35px;
+}
+</style>
